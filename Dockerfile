2aed053dcd4e057c9b5b41c03b68c506318424c3
Nailya Zagrutdinova <nzgr1@mail.ru>
2023-04-24 22:20:14 +0300

    исправления

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ WORKDIR /app
 COPY . .
 RUN mvn verify package
 FROM openjdk:11
-COPY --from=build /app/target/Blockchain-1.0-SNAPSHOT-jar-with-dependencies.jar /usr/local/lib/Blockchain.jar
+COPY --from=build /app/target/BlockChain-1.0-SNAPSHOT-jar-with-dependencies.jar /usr/local/lib/Blockchain.jar
 ENTRYPOINT ["java", "-jar", "/usr/local/lib/Blockchain.jar"]
--- src/main/java/nailya/Main.java
+++ src/main/java/nailya/Main.java
@@ -4,20 +4,14 @@ package nailya;
 public class Main {
     public static void main(String[] args) {
         Blockchain blockchain = new Blockchain();
-        blockchain.addBlock(new Block(0, blockchain.getLatestBlock().getHash(),
-                "This is the first node"));
-        System.out.println(blockchain.getLatestBlock().getData());
-        System.out.println("Is valid? " + blockchain.isChainValid());
-
-        blockchain.addBlock(new Block(1, blockchain.getLatestBlock().getHash(),
-                "This is the second node"));
-        System.out.println(blockchain.getLatestBlock().getData());
-        System.out.println("Is valid? " + blockchain.isChainValid());
-
-        blockchain.addBlock(new Block(2, blockchain.getLatestBlock().getHash(),
-                "This is the third node"));
-        System.out.println(blockchain.getLatestBlock().getData());
-        System.out.println("Is valid? " + blockchain.isChainValid());
+        int i = 0;
+        while (true){
+            blockchain.addBlock(new Block(i, blockchain.getLatestBlock().getHash(),
+                    "This is the "+i+" node"));
+            System.out.println(blockchain.getLatestBlock().getData());
+            System.out.println("Is valid? " + blockchain.isChainValid());
+            i++;
+        }
     }
 }
 
